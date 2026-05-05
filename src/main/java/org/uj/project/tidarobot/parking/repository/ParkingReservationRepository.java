1d24dd7f4fd0d25c45f8648e22ebce798a924d28
Wojtek <wojtekmikula1@gmail.com>
2026-05-05 19:03:55 +0200

    fix(parking reservation repository) - use Join Fetch in query

--- src/main/java/org/uj/project/tidarobot/parking/repository/ParkingReservationRepository.java
+++ src/main/java/org/uj/project/tidarobot/parking/repository/ParkingReservationRepository.java
@@ -1,6 +1,8 @@
 package org.uj.project.tidarobot.parking.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import org.uj.project.tidarobot.parking.entity.ParkingReservation;
 import org.uj.project.tidarobot.parking.entity.ReservationStatus;
@@ -13,5 +15,6 @@ public interface ParkingReservationRepository extends JpaRepository<ParkingReser
 
     List<ParkingReservation> findAllByUserId(Long userId);
 
-    List<ParkingReservation> findAllByStatusAndScheduledForBefore(ReservationStatus status, LocalDateTime threshold);
+    @Query("SELECT r FROM ParkingReservation r JOIN FETCH r.user WHERE r.status = :status AND r.scheduledFor < :threshold")
+    List<ParkingReservation> findAllByStatusAndScheduledForBefore(@Param("status") ReservationStatus status, @Param("threshold") LocalDateTime threshold);
 }
