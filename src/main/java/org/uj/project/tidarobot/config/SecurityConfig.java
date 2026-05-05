64d4bfbea335b5d01e645de7d286f12d8197128e
Wojtek <wojtekmikula1@gmail.com>
2026-05-05 19:05:02 +0200

    fix(security config) - add Cors problem

--- src/main/java/org/uj/project/tidarobot/config/SecurityConfig.java
+++ src/main/java/org/uj/project/tidarobot/config/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:4200"));
+        config.setAllowedOrigins(List.of("http://localhost:4200", "http://localhost"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         config.setAllowCredentials(true);
